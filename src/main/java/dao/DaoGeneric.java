b769c49af65e8d791a23f8f8917892068f9b155f
thiersfb <thiers.fb@gmail.com>
2023-01-04 18:12:34 -0300

    Criado DAO Genérico

---
+++ src/main/java/dao/DaoGeneric.java
@@ -0,0 +1,20 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import projetomavenhibernate.HibernateUtil;
+
+public class DaoGeneric<E> {
+
+	private EntityManager entityManager  = HibernateUtil.getEntityManager();
+	
+	public void salvar(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(entidade);
+		transaction.commit();
+		
+	}
+	
+}
--- src/test/java/projetomavenhibernate/TesteHibernate.java
+++ src/test/java/projetomavenhibernate/TesteHibernate.java
@@ -1,12 +1,50 @@
 package projetomavenhibernate;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
+
+import Model.UsuarioPessoa;
+import dao.DaoGeneric;
 
 public class TesteHibernate {
 	
 	@Test
-	public void testeHibernateUtil() {
-		HibernateUtil.getEntityManager();
+	public void testeHibernateUtil() throws ParseException {
+		//HibernateUtil.getEntityManager();
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		pessoa.setLogin("thiersfb");
+		pessoa.setNome("Thiers");
+		pessoa.setSobrenome("Barizon");
+		pessoa.setEmail("thiers.fb@gmail.com");
+		pessoa.setSenha("thiers07");
+		
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		Date dataNascimento = dateFormat.parse("29-02-1988");
+		pessoa.setDataNascimento(dataNascimento);
+		
+		daoGeneric.salvar(pessoa);
+		
+		UsuarioPessoa pessoa2 = new UsuarioPessoa();
+		pessoa2.setLogin("jupedro");
+		pessoa2.setNome("Juliana");
+		pessoa2.setSobrenome("Pedro");
+		pessoa2.setEmail("jupedro.08@hotmail.com");
+		pessoa2.setSenha("jupedroju");
+		
+		
+		//DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		dataNascimento = dateFormat.parse("06-11-1985");
+		pessoa2.setDataNascimento(dataNascimento);
+		
+		daoGeneric.salvar(pessoa2);
+		
 	}
 	
 }
